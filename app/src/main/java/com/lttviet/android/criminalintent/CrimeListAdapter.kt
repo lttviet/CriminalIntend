65cb552c1e552bd195c3c0ee48e6e5a78b9042a0
lttviet <lttviet@gmail.com>
2018-05-07 22:57:31 -0400

    Nicer date format

--- app/src/main/java/com/lttviet/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/lttviet/android/criminalintent/CrimeListAdapter.kt
@@ -5,32 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.list_item_crime.view.*
-import org.jetbrains.anko.toast
 
-class CrimeListAdapter(var crimes: List<Crime>?): RecyclerView.Adapter<CrimeHolder>() {
+class CrimeListAdapter(private var crimes: List<Crime>?): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun getItemCount(): Int {
         return crimes?.size ?: 0
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
-        val textView = LayoutInflater.from(parent.context)
-                .inflate(R.layout.list_item_crime, parent, false)
-        return CrimeHolder(textView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return CrimeHolder(LayoutInflater.from(parent.context)
+                    .inflate(R.layout.list_item_crime, parent, false))
     }
 
-    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         crimes?.get(position)?.let { crime ->
-            holder.bind(crime)
+            (holder as ViewHolder).bind(crime)
         }
     }
 }
 
-class CrimeHolder(itemView: View): RecyclerView.ViewHolder(itemView)  {
-    fun bind(crime: Crime) {
+interface ViewHolder {
+    fun bind(crime: Crime)
+}
+
+class CrimeHolder(itemView: View): RecyclerView.ViewHolder(itemView), ViewHolder  {
+    override fun bind(crime: Crime) {
         itemView.crime_title.text = crime.title
-        itemView.crime_date.text = crime.date.toString()
-        itemView.setOnClickListener { _ ->
-            itemView.context.toast(crime.title + " clicked!")
-        }
+        itemView.crime_date.text = android.text.format.DateFormat
+                .format("EEEE, MMM d, yyyy", crime.date)
+        itemView.crime_solved.visibility = if (crime.isSolved) View.VISIBLE else View.INVISIBLE
     }
 }
